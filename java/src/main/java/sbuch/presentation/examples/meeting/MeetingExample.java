bb6dd18c5596e246a29e14a1a0c2e8224fe3f663
Sébastien Buchoux <s.buchoux@groupeonepoint.com>
2025-06-26 08:57:39 +0200

    feat: Add usage example for java

--- java/src/main/java/sbuch/presentation/examples/meeting/MeetingExample.java
+++ java/src/main/java/sbuch/presentation/examples/meeting/MeetingExample.java
@@ -5,21 +5,34 @@ import sbuch.presentation.examples.meeting.framework.MeetingFramework;
 
 import java.net.URL;
 
+import org.jboss.logging.Logger;
+
 public class MeetingExample {
+    private static Logger LOGGER = Logger.getLogger(MeetingExample.class);
 
     private MeetingExample() {
     }
 
-    public static ValidationResult<Meeting> createMeetingFromFrameworkValidated(MeetingFramework frameworkValidated) {
+    public static ValidationResult<Meeting> createMeetingFromFrameworkValidated(
+            MeetingFramework frameworkValidated) {
         return switch (frameworkValidated) {
             case MeetingFramework.InPersonMeetingFramework(String title, String email, MeetingRoom room, int nGuests) ->
-                    InPersonMeeting.of(
-                            title, email, room, nGuests
-                    );
+                InPersonMeeting.of(
+                        title, email, room, nGuests);
             case MeetingFramework.OnlineMeetingFramework(String title, String email, VideoPlatform platform, URL url) ->
-                    OnlineMeeting.of(
-                            title, email, platform, url
-                    );
+                OnlineMeeting.of(
+                        title, email, platform, url);
         };
+    }
+
+    private void saveToRepo(Meeting meeting) {
+        switch (meeting) {
+            case InPersonMeeting:
+                
+                break;
+        
+            default:
+                break;
+        }LOGGER.info("saving meeting booked by %s to whatever DB you want".format(meeting.));
     }
 }
