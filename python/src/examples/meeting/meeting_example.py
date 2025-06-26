492e248868fa4f4c0256a761fa0a61579c469c2a
Sébastien Buchoux <s.buchoux@groupeonepoint.com>
2025-06-26 08:47:19 +0000

    feat: Add meeting usage for Python exmaple

--- python/src/examples/meeting/meeting_example.py
+++ python/src/examples/meeting/meeting_example.py
@@ -45,3 +45,17 @@ def create_meeting_from_raw(raw_object: Any) -> Meeting | ValidationError:
         return validation_result
 
     return create_meeting_from_framework_validated(validation_result)
+
+
+def get_n_free_seats(meeting: InPersonMeeting) -> int:
+    match meeting.meeting_room:
+        case "Room-101":
+            capacity = 20
+        case "Room-42":
+            capacity = 50
+        case "Amphitheater":
+            capacity = 200
+        case _:
+            assert_never(meeting.meeting_room)
+
+    return capacity - meeting.n_guests
