de057b7abe3371812067248d606f24f3fb2edb03
Sébastien Buchoux <s.buchoux@groupeonepoint.com>
2025-06-27 11:34:46 +0200

    feat: Add usage example for java

--- java/src/main/java/sbuch/presentation/examples/meeting/MeetingExample.java
+++ java/src/main/java/sbuch/presentation/examples/meeting/MeetingExample.java
@@ -4,35 +4,37 @@ import sbuch.presentation.examples.meeting.core.*;
 import sbuch.presentation.examples.meeting.framework.MeetingFramework;
 
 import java.net.URL;
-
-import org.jboss.logging.Logger;
+import java.util.logging.Logger;
 
 public class MeetingExample {
-    private static Logger LOGGER = Logger.getLogger(MeetingExample.class);
+    private static final Logger LOGGER = Logger.getLogger(MeetingExample.class.getName());
 
     private MeetingExample() {
     }
 
-    public static ValidationResult<Meeting> createMeetingFromFrameworkValidated(
-            MeetingFramework frameworkValidated) {
+    public static ValidationResult<Meeting> createMeetingFromFrameworkValidated(MeetingFramework frameworkValidated) {
         return switch (frameworkValidated) {
             case MeetingFramework.InPersonMeetingFramework(String title, String email, MeetingRoom room, int nGuests) ->
-                InPersonMeeting.of(
-                        title, email, room, nGuests);
+                    InPersonMeeting.of(
+                            title, email, room, nGuests
+                    );
             case MeetingFramework.OnlineMeetingFramework(String title, String email, VideoPlatform platform, URL url) ->
-                OnlineMeeting.of(
-                        title, email, platform, url);
+                    OnlineMeeting.of(
+                            title, email, platform, url
+                    );
         };
     }
 
-    private void saveToRepo(Meeting meeting) {
+    public void saveToRepo(Meeting meeting) {
         switch (meeting) {
-            case InPersonMeeting:
-                
+            case InPersonMeeting inPersonMeeting: {
+                LOGGER.info("InPersonMeeting in room %s saved to DB".formatted(inPersonMeeting.meetingRoom));
                 break;
-        
-            default:
-                break;
-        }LOGGER.info("saving meeting booked by %s to whatever DB you want".format(meeting.));
+            }
+            case OnlineMeeting onlineMeeting: {
+                LOGGER.info("OnlineMeeting hosted on %s saved to DB".formatted(onlineMeeting.videoPlatform));
+            }
+        }
     }
+
 }
